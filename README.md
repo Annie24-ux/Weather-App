aa934b9abeccfda97d9abd34a0b5d9a92cb204d7
Siphesihle Sibiya <ssibiya023@student.wethinkcode.co.za>
2024-09-02 19:37:53 +0200

    Weatherly app update.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Weather-App
-Website to show weather forecast
+
+---
+
+## Weather App
+Welcome to the Weatherly app!
+
+### Description
+This Weather App allows you to check the current weather conditions by entering the name of any city. The app provides the following details:
+
+- **Temperature**: Current temperature in the city.
+- **Weather Icon**: A visual representation of the weather conditions (e.g., sunny, cloudy, rainy).
+- **Date**: The date and time of the weather data.
+- **Humidity**: The percentage of humidity in the air.
+- **Wind Speed**: The speed of the wind in the selected city.
+
+Simply input the city name, and the app will retrieve and display these weather details.
+
+---
--- src/index.js
+++ src/index.js
@@ -1,58 +1,78 @@
-function showWeatherChange(response) {
-    let currentCity = document.querySelector(".current-city");
-    let newCity = response.data.city;
-  
-    currentCity.innerHTML = newCity;
-  
-    let currentTemp = document.querySelector(".current-temperature-value");
-    let newTemp = Math.round(response.data.temperature.current);
-  
-    currentTemp.innerHTML = newTemp;
-  }
-  
-  function search(event) {
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#search-input");
-    let cityElement = searchInputElement.value;
-  
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = Math.round(response.data.temperature.current);
+
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = temperature;
+
+    let descriptionElement = document.querySelector("#description");
+    let description = response.data.condition.description;
+    descriptionElement.innerHTML = ", " + description;
+
+
+    let humidElement = document.querySelector("#humid");
+    let humid = response.data.temperature.humidity;
+    humidElement.innerHTML = `${humid} %`;
+
+    let windElement = document.querySelector("#wind");
+    let wind = response.data.wind.speed;
+    windElement.innerHTML = `${wind}  km/h`;
+
+    let timeElement = document.querySelector("#time");
+    let weatherDate = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(weatherDate);
+
+    let iconElement = document.querySelector("#weather-icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+    // let icon = response.data.condition.icon;
+    // iconElement.innerHTML = icon;
+
+    function formatDate(date){
+
+        let weekDays = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let day = weekDays[date.getDay()];
+
+        if(minutes < 10){
+            minutes = `0${minutes}`;
+        }
+
+        return `${day} ${hours}:${minutes}`;
+
+
+
+
+    }
+}
+
+
+
+function citySearch(city){
     let apiKey = "b2a5adcct04b33178913oc335f405433";
-    let weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement}&key=${apiKey}&units=metric`;
-  
-    axios.get(weatherUrl).then(showWeatherChange);
-  }
-  
-  function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let day = date.getDay();
-  
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-  
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-  
-    let formattedDay = days[day];
-    return `${formattedDay} ${hours}:${minutes}`;
-  }
-  
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", search);
-  
-  let currentDateELement = document.querySelector("#current-date");
-  let currentDate = new Date();
-  
-  currentDateELement.innerHTML = formatDate(currentDate);
-  
+    let weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(weatherUrl);
+
+    axios.get(weatherUrl).then(refreshWeather);
+}
+
+
+function searchSubmitHandler(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#form-input");
+
+    citySearch(searchInput.value);
+}
+
+
+
+
+
+let searchFormElement = document.querySelector("#answer");
+searchFormElement.addEventListener("click", searchSubmitHandler);
+
+citySearch("Malelane");
