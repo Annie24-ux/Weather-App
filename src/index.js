964e823c65932b71307e0cb06e8a3d31e0d9fad8
Siphesihle <ssibiya023@student.wethinkcode.co.za>
2024-09-03 00:13:27 +0200

    Hour format returned

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ function refreshWeather(response){
             minutes = `0${minutes}`;
         }
 
+        if(hours < 10){
+            hours = `0${hours}`;
+        }
+
         return `${day} ${hours}:${minutes}`;
 
 
